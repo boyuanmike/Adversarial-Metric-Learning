4a97bdc912a82e91707be220f2d9fb54fb8f4ba2
kevhe <kevhe@umich.edu>
2019-04-09 20:52:13 -0400

    implement train_eval.py

--- common/train_eval.py
+++ common/train_eval.py
@@ -22,7 +22,11 @@ from ..datasets.dataset import *
 from ..models.modified_googlenet import ModifiedGoogLeNet
 from ..models.net import Generator, Discriminator
 
-def train(main_script_path, func_train_one_batch, param_dict, savev_distance_matrix=False,):
+def get_optimizer(model):
+	optimizer = optim.Adam(model.parameters())
+	return optimizer
+
+def train(main_script_path, func_train_one_batch, param_dict, savev_distance_matrix=False, path = None):
 	script_filename = os.path.splitext(os.path.basename(main_script_path))[0]
 
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -33,15 +37,103 @@ def train(main_script_path, func_train_one_batch, param_dict, savev_distance_mat
 	p = utils.Logger(log_dir_path, **param_dict)
 
 	# load data base
-
-	streams = data_provider.get_streams(p.batch_size, dataset = p.dataset, 
-		method = p.method, crop_size = p.crop_size)
-	stream_train, stream_train_eval, stream_test = streams
-	iter_train = stream_train.get_epoch_iterator()
-
+	if p.dataset is 'cars196':
+		data = Car196(root = path)
+	else:
+		data = CUB_200_2011(root = path)
+	train_loader = torch.utils.data.DataLoader(data.train, batch_size = p.batch_size)
+	train_it = iter(train_loader)
+	test_loader = torch.utils.data.DataLoader(data.test, batch_size = p.batch_size)
 	# construct the model
 
 	model = ModifiedGoogLeNet(p.out_dim, p.normalize_output)
 	model_gen = Generator()
 	model_dis = Discriminator(512, 512)
-	model.
+	
+	model_optimizer = get_optimizer(model)
+	gen_optimizer = get_optimizer(model_gen)
+	dis_optimizer = get_optimizer(model_dis)
+	model_feat_optimizer = get_optimizer(model)
+
+	stop = False
+	logger = utils.Logger(log_dir_path)
+	logger.soft_test_best = [0]
+	time_origin = time.time()
+	best_nmi_1 = 0.
+	best_f1_1 = 0.
+	best_nmi_2 = 0.
+	best_f1_2 = 0.
+
+	for epoch in range(p.num_epochs):
+		time_begin = time.time()
+		epoch_loss_gen = []
+		epoch_loss_dis = []
+		loss = 0
+		t = tqdm(range(p.num_batches_per_epoch))
+		for i in t:
+			t.set_description(desc='# {}'.format(epoch))
+			model_optimizer.zero_grad()
+			gen_optimizer.zero_grad()
+			dis_optimizer.zero_grad()
+			model_feat_optimizer.zero_grad()
+			loss_gen, loss_dis = func_train_one_batch(model, model_gen, model_dis, 
+				model_optimizer, model_feat_optimizer, gen_optimizer, dis_optimizer, p, next(train_it), 
+				epoch)
+			epoch_loss_gen.append(loss_gen.item())
+			epoch_loss_dis.append(loss_dis.item())
+
+		loss_average_gen = sum(epoch_loss_gen) / float(len(epoch_loss_gen))
+		loss_average_dis = sum(epoch_loss_dis) / float(len(epoch_loss_dis))
+
+		D = [0]
+		soft = [0]
+		hard = [0]
+		retrieval = [0]
+
+		nmi, f1 = evaluate(
+			model, model_dis, iter(test_loader), p.distance_type,
+			return_distance_matrix = savev_distance_matrix, epoch = epoch)
+		if nmi > best_nmi_1:
+			best_nmi_1 = nmi
+			best_f1_1 = f1
+			torch.save(model, os.getcwd() + '/model.pt')
+			torch.save(model_gen, os.getcwd() + '/model_gen.pt')
+			torch.save(model_dis, os.getcwd() + '/model_dis.pt')
+		if f1 > best_f1_2:
+			best_nmi_2 = nmi
+			best_f1_2 = f1
+
+		time_end = time.time()
+		epoch_time = time_end - time_begin
+		total_time = time_end - time_origin
+
+		logger.epoch = epoch
+		logger.total_time = total_time
+		logger.gen_loss_log.append(loss_average_gen)
+		logger.dis_loss_log.append(loss_average_dis)
+		logger.train_log.append([soft[0], hard[0], retrieval[0]])
+		print("#", epoch)
+		print("time: {} ({})".format(epoch_time, total_time))
+		print("[train] loss gen:", loss_average_gen)
+		print("[train] loss dis:", loss_average_dis)
+		print("[test]  nmi:", nmi)
+		print("[test]  f1:", f1)
+		print("[test]  nmi:", best_nmi_1, "  f1:", best_f1_1, "for max nmi")
+		print("[test]  nmi:", best_nmi_2, "  f1:", best_f1_2, "for max f1")
+		print(p)
+
+	dir_name = "-".join([p.dataset, script_filename,
+		time.strftime("%Y%m%d%H%M%S"), str(logger.soft_test_best[0])])
+
+	logger.save(dir_name)
+	p.save(dir_name)
+
+	print("total epochs: {} ({} [s])".format(logger.epoch, logger.total_time))
+    print("best test score (at # {})".format(logger.epoch_best))
+    print("[test]  soft:", logger.soft_test_best)
+    print("[test]  hard:", logger.hard_test_best)
+    print("[test]  retr:", logger.retrieval_test_best)
+    print(str(p).replace(', ', '\n'))
+    print()
+    
+    return stop
