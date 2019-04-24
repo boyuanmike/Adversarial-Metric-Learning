43f38d273c99dc173b40d411465a1550132000ea
sparkroy <royluo@umich.edu>
2019-04-23 20:29:19 -0400

    Add files via upload

--- main_2gen.py
+++ main_2gen.py
@@ -1,42 +1,39 @@
 import colorama
 
-# from common.train_eval import train
-# from common.utils import lossfun_one_batch, lossfun_one_batch_baseline
-from common.train_eval_2gen import train
-from common.utils_2gen import lossfun_one_batch,lossfun_one_batch_retain
+from common.train_eval import train
+from common.utils import lossfun_one_batch, lossfun_one_batch_baseline, lossfun_one_batch_retain
 
 colorama.init()
-path = '/disk-main/'
-# ds = 'CUB_200_2011'
-ds = 'car196'
 
+path = '/disk-main/'
+#ds = 'CUB_200_2011'
+ds='car196'
 def main():
     static_params = dict(
         n_samples=5,
         num_epochs=100,
-        batch_size=64,
+        batch_size=32,
         out_dim=512,
         crop_size=224,
         normalize_output=True,
         normalize_hidden=False,
         distance_type='euclidean',
-        dataset='car196',
+        dataset=ds,
         l2_weight_decay=1e-4,
         alpha=1,
         learning_rate=1e-3,
         n_classes=10,
-        neg_gen_epoch=0,
+        neg_gen_epoch=10,
         model_save_path=path + "models"
     )
-
-
+    # train(lossfun_one_batch, static_params, '/disk-main/car196', "/disk-main/logs/")
     data_path = path + ds
-
+    #log_path = "logs/"+ ds +'/'
     log_path = "logs/"+ ds +'_retain/'
 
-    train(lossfun_one_batch, static_params, data_path, log_path)
+    train(lossfun_one_batch_retain, static_params, data_path, log_path)
 
-    # train(lossfun_one_batch_baseline, static_params, data_path, log_path)
+    #train(lossfun_one_batch_baseline, static_params, data_path, log_path)
 
 
 
